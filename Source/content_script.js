f06e6cfdfd304fa9ed0a94707624ea0ac6854962
Joel Whang <joelwhang.pdx@gmail.com>
2024-12-17 16:32:35 -0500

    Refactor for readability. Rename TTBlocker to TTPatrol.

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,41 +1,55 @@
-const naughtylist = ["trip", "travel", "flight", "plane", "jetlag", "timezone"];
+const MIN_TEXT_LENGTH = 4;
 
-// this is kind of slow and will only scale
-function is_naughty(str) {
-  const normalized = str.trim().toLowerCase();
-  return naughtylist.some((naughty_word) => str.match(naughty_word));
-}
-
-// This mutation observer revists the same document 20x times. Find out
-// a way if this can be reduced.
-const callback = (mutationList, observer) => {
-  mutationList.forEach((record) => {
-    record.addedNodes.forEach((node) => {
-      requestIdleCallback(() => search_and_redact(node));
-    });
-  });
-};
-
+// Set up MutationObserver to monitor added nodes.
 const config = {
   attributes: false,
   childList: true,
   subtree: true,
 };
+const callback = (mutationList, observer) => {
+  mutationList.forEach((record) => {
+    record.addedNodes.forEach((node) => {
+      // Defer patrol() because TreeWalking and Regex matching are expensive.
+      requestIdleCallback(() => patrol(node));
+    });
+  });
+};
 const observer = new MutationObserver(callback);
 observer.observe(document.body, config);
 
-function search_and_redact(el) {
-  function countWords(str) {
-    return str.trim().split(" ").length;
-  }
-  const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null);
+// Search for all subnodes of Text type to be redacted.
+function patrol(node) {
+  const walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null);
   while (walker.nextNode()) {
-    if (walker.currentNode.parentElement.tagName.toLowerCase() != "script") {
-      node = walker.currentNode;
-      if (countWords(node.nodeValue) > 4 && is_naughty(node.nodeValue)) {
-        console.log("blocked: " + node.nodeValue);
-        node.nodeValue = "redacted";
-      }
+    if (is_violation(walker.currentNode)) {
+      redact(walker.currentNode);
     }
   }
+}
+
+// Patrol helper functions
+const naughtylist = ["trip", "travel", "flight", "plane", "jetlag", "timezone"];
+
+// Returns true if given string contains a word in the naughtylist.
+function is_naughty(str) {
+  const normalized = str.trim().toLowerCase();
+  return naughtylist.some((naughty_word) => normalized.match(naughty_word));
+}
+
+function is_violation(node) {
+  function long_enough(str) {
+    return str.trim().split(" ").length > MIN_TEXT_LENGTH;
+  }
+  return (
+    node.parentElement.tagName.toLowerCase() != "script" &&
+    long_enough(node.nodeValue) &&
+    is_naughty(node.nodeValue)
+  );
+}
+
+// Redact violating node.
+function redact(node) {
+  console.log(`Redacting: "${node.nodeValue}"`);
+  // TODO(jwhang): Make more visually smart.
+  node.nodeValue = "redacted";
 }

7b9d035c0f68e3f040f71a41dcd2c1f97c3896bb
Joel Whang <joelwhang.pdx@gmail.com>
2024-12-25 19:33:07 -0800

    Identify images from the page.

--- Source/content_script.js
+++ Source/content_script.js
@@ -17,6 +17,8 @@ const MIN_NUM_WORDS = 4; // Text must be at least MIN_NUM_WORDS to be analyzed f
 const REDACTED_MSG = "Redacted: ";
 const CHAT_ID = 1621389464744799;
 
+images = [];
+
 // Set up MutationObserver to monitor added nodes.
 const config = {
   attributes: false,
@@ -26,7 +28,7 @@ const config = {
 const callback = (mutationList, observer) => {
   mutationList.forEach((record) => {
     // Only patrol if
-    if (is_las_chicas(window.location)) {
+    if (isLasChicas(window.location)) {
       record.addedNodes.forEach((node) => {
         // Defer patrol() because TreeWalking and Regex matching are expensive.
         requestIdleCallback(() => patrol(node));
@@ -34,26 +36,52 @@ const callback = (mutationList, observer) => {
     }
   });
 };
+
 const observer = new MutationObserver(callback);
 observer.observe(document.body, config);
 
 // Search for all subnodes of Text type which may need to be redacted.
 function patrol(node) {
+  patrolImages(node);
+  patrolText(node);
+}
+
+// Patrol helper functions
+
+function patrolImages(node) {
+  MIN_HEIGHT = 100;
+  MIN_WIDTH = 100;
+  var imgs = node.getElementsByTagName("img");
+  Array.from(imgs).forEach((img) => {
+    if (img.visited === true) {
+      return;
+    }
+
+    // Discard small images.
+    if (img.height < MIN_HEIGHT || img.width < MIN_WIDTH) {
+      return;
+    }
+
+    console.log(img.src);
+    img.visited = true;
+  });
+}
+
+function patrolText(node) {
   const walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null);
   while (walker.nextNode()) {
-    if (is_redacted(walker.currentNode)) {
+    if (isRedacted(walker.currentNode)) {
+      console.log("Skipping redacted material");
       continue;
     }
-    if (is_violation(walker.currentNode)) {
+    if (isViolation(walker.currentNode)) {
       redact(walker.currentNode);
     }
   }
 }
 
-// Patrol helper functions
-
 // Returns true if given string should be redacted.
-function is_violation(node) {
+function isViolation(node) {
   function valid_element(node) {
     return node.parentElement.tagName.toLowerCase() != "script";
   }
@@ -81,10 +109,12 @@ function redact(node) {
   node.parentNode.style.backgroundColor = "black";
 }
 
-function is_redacted(node) {
+function isRedacted(node) {
+  // TODO(jwhang): This is kind of broken and needs to be fixed.
+  // It does not prevent re-redacting redacted messages.
   node.nodeValue.startsWith(REDACTED_MSG);
 }
 
-function is_las_chicas() {
+function isLasChicas() {
   return location.pathname == "/t/" + CHAT_ID;
 }

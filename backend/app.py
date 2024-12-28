65116e55dd0f15ae18a70b2fb5bbb7d7dfc464f9
Joel Whang <joelwhang.pdx@gmail.com>
2024-12-28 15:41:54 -0800

    Make app.py use gunicorn. App makes call to openai.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+import logging
 from os import environ
 
 from flask import Flask, jsonify, make_response, request
 from flask_sqlalchemy import SQLAlchemy
+
+from analyze_image import ImageAnalysisClient
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DB_URL")
@@ -11,14 +14,26 @@ db = SQLAlchemy(app)
 class Image(db.Model):
     __tablename__ = "images"
 
-    name = db.Column(db.String(600), unique=True, nullable=False, primary_key=True)
-    is_violation = db.Column(db.Boolean, unique=True, nullable=False)
+    image_url = db.Column(
+        db.String(1000), unique=True, nullable=False, primary_key=True
+    )
+    is_violation = db.Column(db.Boolean, unique=False, nullable=False)
 
     def json(self):
-        return {"name": self.name, "is_violation": self.is_violation}
+        return {"image_url": self.image_url, "is_violation": self.is_violation}
 
 
-db.create_all()
+def setup_logging():
+    # if not app.debug:
+    if True:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
+
+
+with app.app_context():
+    db.create_all()
+    setup_logging()
 
 
 # create a test route
@@ -32,12 +47,22 @@ def test():
 def create_image():
     try:
         data = request.get_json()
-        new_image = Image(name=data["name"], is_violation=data["is_violation"])
+        image_url = data["image_url"]
+        app.logger.info("JWDEBUG: " + image_url)
+
+        openai_client = ImageAnalysisClient()
+        image_analysis = openai_client.analyze_image(image_url)
+
+        new_image = Image(
+            image_url=data["image_url"], is_violation=image_analysis.is_travel_image
+        )
         db.session.add(new_image)
         db.session.commit()
         return make_response(jsonify({"message": "image created"}), 201)
-    except e:
-        return make_response(jsonify({"message": "error creating image"}), 500)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": f"error creating image", "exception": str(e)}), 500
+        )
 
 
 # get all images
@@ -46,47 +71,55 @@ def get_images():
     try:
         images = Image.query.all()
         return make_response(jsonify([image.json() for image in images]), 200)
-    except e:
-        return make_response(jsonify({"message": "error getting images"}), 500)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": f"error getting images", "exception": str(e)}), 500
+        )
 
 
-# get a image by name
-@app.route("/images/<name>", methods=["GET"])
-def get_image(name):
+# get a image by image_url
+@app.route("/images/<path:image_url>", methods=["GET"])
+def get_image(image_url):
     try:
-        image = Image.query.filter_by(name=name).first()
+        image = Image.query.filter_by(image_url=image_url).first()
         if image:
             return make_response(jsonify({"image": image.json()}), 200)
         return make_response(jsonify({"message": "image not found"}), 404)
-    except e:
-        return make_response(jsonify({"message": "error getting image"}), 500)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": f"error getting image", "exception": str(e)}), 500
+        )
 
 
 # update a image
-@app.route("/images/<name>", methods=["PUT"])
-def update_image(name):
+@app.route("/images/<path:image_url>", methods=["PUT"])
+def update_image(image_url):
     try:
-        image = Image.query.filter_by(name=name).first()
+        image = Image.query.filter_by(image_url=image_url).first()
         if image:
             data = request.get_json()
-            image.name = data["name"]
+            image.image_url = data["image_url"]
             image.is_violation = data["is_violation"]
             db.session.commit()
             return make_response(jsonify({"message": "image updated"}), 200)
         return make_response(jsonify({"message": "image not found"}), 404)
-    except e:
-        return make_response(jsonify({"message": "error updating image"}), 500)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": f"error updating image", "exception": str(e)}), 500
+        )
 
 
 # delete a image
-@app.route("/images/<name>", methods=["DELETE"])
-def delete_image(name):
+@app.route("/images/<path:image_url>", methods=["DELETE"])
+def delete_image(image_url):
     try:
-        image = Image.query.filter_by(name=name).first()
+        image = Image.query.filter_by(image_url=image_url).first()
         if image:
             db.session.delete(image)
             db.session.commit()
             return make_response(jsonify({"message": "image deleted"}), 200)
         return make_response(jsonify({"message": "image not found"}), 404)
-    except e:
-        return make_response(jsonify({"message": "error deleting image"}), 500)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": f"error deleting image", "exception": str(e)}), 500
+        )
--- backend/practice_client.py
+++ backend/practice_client.py
@@ -47,37 +47,37 @@ completion = client.beta.chat.completions.parse(
 
 end = time.time()
 print(f"Completion with explanation: {end-start}")
-print(completion.choices[0].message)
+print(completion.choices[0].message.parsed.is_travel_image)
 
 
-start = time.time()
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "developer",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "You analyze images and answer only 'true' or 'false': is the image related to travel? This includes, but is not limited to being in an airplane or at an airport, eating food or drinking somewhere abroad, or photos of general sightseeing views.",
-                },
-            ],
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": image_url,
-                    },
-                },
-            ],
-        },
-    ],
-)
-end = time.time()
-print(f"Completion without explanation: {end-start}")
-
-
-print(completion.choices[0].message)
+# start = time.time()
+# completion = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {
+#             "role": "developer",
+#             "content": [
+#                 {
+#                     "type": "text",
+#                     "text": "You analyze images and answer only 'true' or 'false': is the image related to travel? This includes, but is not limited to being in an airplane or at an airport, eating food or drinking somewhere abroad, or photos of general sightseeing views.",
+#                 },
+#             ],
+#         },
+#         {
+#             "role": "user",
+#             "content": [
+#                 {
+#                     "type": "image_url",
+#                     "image_url": {
+#                         "url": image_url,
+#                     },
+#                 },
+#             ],
+#         },
+#     ],
+# )
+# end = time.time()
+# print(f"Completion without explanation: {end-start}")
+#
+#
+# print(completion.choices[0].message)

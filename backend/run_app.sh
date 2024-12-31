c01484bdedb2ccb4cd20ada21eae9598d2d6be7f
Joel Whang <joelwhang.pdx@gmail.com>
2024-12-31 15:44:51 -0500

    Fix bash script

--- backend/run_app.sh
+++ backend/run_app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo FLASK_ENV = $FLASK_ENV
-if [[ "$FLASK_ENV" -eq "deveopment" ]]; then
+if [[ "$FLASK_ENV" == "development" ]]; then
         flask --debug run --host=0.0.0.0 --port=4000
 fi
 
-if [[ "$FLASK_ENV" -eq "production" ]]; then
+if [[ "$FLASK_ENV" == "production" ]]; then
         gunicorn --bind 0.0.0.0:4000 app:app
 fi

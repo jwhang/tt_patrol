cbe90dcf8d0b476799b1079e90d0153293c0c7af
Joel Whang <joelwhang.pdx@gmail.com>
2025-01-02 22:06:44 -0500

    Cache backend responses for image analysis on client side.

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,9 +1,21 @@
 import { BACKEND_URL } from "./constants"
+import { LRUCache } from 'lru-cache'
+
+const cache = new LRUCache({
+  max: 100,  // max size
+});
 
 // TODO(jwhang): Need a client side cache
 chrome.runtime.onMessage.addListener(
   function(message, sender, senderResponse) {
     let resourceUrl = `${BACKEND_URL}/images/${encodeURIComponent(message.url)}`;
+    let cached_resp = cache.get(resourceUrl);
+    if (cached_resp !== undefined) {
+      console.log("Serving cached resp");
+      senderResponse(cached_resp)
+      return true;
+    }
+
     fetch(resourceUrl, {
       method: "PUT",
     })
@@ -11,6 +23,7 @@ chrome.runtime.onMessage.addListener(
         return res.json();
       })
       .then((res) => {
+        cache.set(resourceUrl, res);
         senderResponse(res);
       });
     return true;
